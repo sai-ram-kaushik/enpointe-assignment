8751c3f7345cfda1a2ac10149805263e732a9679
sai-ram-kaushik <sairamkaushik1@gmail.com>
2024-07-17 23:00:16 +0530

    updated the ratings on home page

--- src/js/index.js
+++ src/js/index.js
@@ -23,8 +23,29 @@ fetch("https://api.themoviedb.org/3/movie/popular?language=en-US&page=1", {
 
       const imgUrl = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
       movieDiv.innerHTML = `
-         <img src="${imgUrl}" class="rounded-xl cursor-pointer" />
-         <p class="py-3 font-bold text-[16px]">${movie.title}</p>
+         <div class="relative">
+          <img src="${imgUrl}" class="rounded-xl cursor-pointer relative" />
+          <div
+          class="flex items-center gap-3 bg-black/50 p-2 rounded-xl text-orange-400 absolute top-0"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="{1.5}"
+            stroke="currentColor"
+            class="size-5"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z"
+            />
+          </svg>
+          <p class="text-lg">${movie.vote_average}</p>
+        </div>
+          <p class="py-3 font-bold text-[16px]">${movie.title}</p>
+         </div>
        `;
 
       movieDiv.addEventListener("click", function () {
